ade073e02c71d316f1c36cf8a11052087ccd0a45
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-09-05 16:42:13 +0200

    Added static root for settings.

--- src/abizeitung_site/settings.py
+++ src/abizeitung_site/settings.py
@@ -65,8 +65,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "abizeitung", "static")
-STATIC_ROOT = ""
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
