bb1db304f3d9eb1b9f936c53012e3d4a640512ce
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-09-05 15:53:32 +0200

    Replaced all ugly '' with beautiful "".

--- src/abizeitung_site/settings.py
+++ src/abizeitung_site/settings.py
@@ -6,20 +6,20 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ("Your Name", "your_email@example.com"),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'abizeitung',                      # Or path to database file if using sqlite3.
+    "default": {
+        "ENGINE": "django.db.backends.mysql", # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "NAME": "abizeitung",                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'abizeitung',
-        'PASSWORD': 'V6z937E7d6',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        "USER": "abizeitung",
+        "PASSWORD": "V6z937E7d6",
+        "HOST": "localhost",                      # Empty for localhost through domain sockets or "127.0.0.1" for localhost through TCP.
+        "PORT": "",                      # Set to empty string for default.
     }
 }
 
@@ -31,11 +31,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = "Europe/Berlin"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'De-de'
+LANGUAGE_CODE = "De-de"
 
 SITE_ID = 1
 
@@ -52,72 +52,72 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = ""
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = ""
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 # Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Don"t put anything in this directory yourself; store your static files
+# in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "abizeitung", "static")
 STATIC_ROOT = ""
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, "abizeitung", "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    # Don"t forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#    "django.contrib.staticfiles.finders.DefaultStorageFinder",
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '&)5h5$7hcm5rm37n^#_5&loaveel55ay1wv%w+z&ps0%vi&!vz'
+# Make this unique, and don"t share it with anybody.
+SECRET_KEY = "&)5h5$7hcm5rm37n^#_5&loaveel55ay1wv%w+z&ps0%vi&!vz"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+#     "django.template.loaders.eggs.Loader",
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
-ROOT_URLCONF = 'abizeitung_site.urls'
+ROOT_URLCONF = "abizeitung_site.urls"
 
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'abizeitung_site.wsgi.application'
+# Python dotted path to the WSGI application used by Django"s runserver.
+WSGI_APPLICATION = "abizeitung_site.wsgi.application"
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'abizeitung', 'templates'),
+    os.path.join(PROJECT_ROOT, "abizeitung", "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    # Don"t forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -131,18 +131,18 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    #'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'south',
-    'abizeitung',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    #"django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "south",
+    "abizeitung",
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    "django.contrib.admin",
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    # "django.contrib.admindocs",
 )
 
 LOGIN_REDIRECT_URL = "/student"
@@ -155,25 +155,25 @@ LOGOUT_URL = "/logout"
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse"
         }
     },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler"
         }
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
     }
 }
--- src/abizeitung_site/settings_production.py
+++ src/abizeitung_site/settings_production.py
@@ -4,14 +4,14 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'abizeitung',                      # Or path to database file if using sqlite3.
+    "default": {
+        "ENGINE": "django.db.backends.mysql", # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "NAME": "abizeitung",                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'abizeitung',
-        'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        "USER": "abizeitung",
+        "PASSWORD": "",
+        "HOST": "localhost",                      # Empty for localhost through domain sockets or "127.0.0.1" for localhost through TCP.
+        "PORT": "",                      # Set to empty string for default.
     }
 }
 
