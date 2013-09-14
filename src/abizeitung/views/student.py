52c110b6b17602aee3302a1e5c44e56537fdcddf
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-09-14 12:35:01 +0200

    Implemented saving the surveys.

--- src/abizeitung/views/student.py
+++ src/abizeitung/views/student.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from abizeitung.models import Teacher, StudentSurvey, Student, TeacherSurvey
+from abizeitung.models import Teacher, StudentSurvey, Student, TeacherSurvey,\
+    StudentSurveyEntry, TeacherSurveyEntry
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.forms import Select, ValidationError
@@ -29,40 +30,95 @@ class StudentEditForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(StudentEditForm, self).__init__(*args, **kwargs)
         
+        self.instance = kwargs["instance"]
+        
         self.fields["test"].widget.attrs["class"] = "form-control"
         
         self.student_choices = [(-1, u"Bitte jemanden auswählen."), ("42", "Test")]
         for student in Student.objects.all():
-            self.student_choices.append((student.id, student.fullname()))
+            self.student_choices.append((student.pk, student.fullname()))
         self.teacher_choices = [(-1, u"Bitte jemanden auswählen.")]
         for teacher in Teacher.objects.all():
-            self.teacher_choices.append((teacher.id, teacher.fullname()))
+            self.teacher_choices.append((teacher.pk, teacher.fullname()))
        
         kwargs = lambda survey: {
             "label" : survey.question,
             "widget" : Select(attrs={"class" : "form-control"}),
         }
 
-        self.student_surveys = []
+        self.student_surveys = {}
         for survey in StudentSurvey.objects.all():
-            field = ChoiceField(choices=self.student_choices, validators=[validate_student], **kwargs(survey))
+            initial = -1
+            entries = StudentSurveyEntry.objects.filter(student=self.instance, survey=survey)
+            if entries.exists():
+                initial = entries[0].choice.pk
+            
+            field = ChoiceField(choices=self.student_choices, initial=initial, validators=[validate_student], **kwargs(survey))
             name = "student_survey_%s" % survey.id
             self.fields[name] = field
-            self.student_surveys.append(self.fields[name])
+            self.student_surveys[survey] = name
         
-        self.teacher_surveys = []
+        self.teacher_surveys = {}
         for survey in TeacherSurvey.objects.all():
-            field = ChoiceField(choices=self.teacher_choices, validators=[validate_teacher], **kwargs)
+            initial = -1
+            entries = TeacherSurveyEntry.objects.filter(student=self.instance, survey=survey)
+            if entries.exists():
+                initial = entries[0].choice.pk
+            
+            field = ChoiceField(choices=self.teacher_choices, initial=initial, validators=[validate_teacher], **kwargs(survey))
             name = "teacher_survey_%s" % survey.id
             self.fields[name] = field
-            self.teacher_surveys.append(name)
+            self.teacher_surveys[survey] = name
 
     def clean(self):
         super(StudentEditForm, self).clean()
 
-        print self.cleaned_data
+        self.survey2student = {}
+        for survey, name in self.student_surveys.items():
+            pk = self.cleaned_data.get(name, "-1")
+            if pk != "-1":
+                self.survey2student[survey] = Student.objects.get(pk=self.cleaned_data[name])
+        
+        self.survey2teacher = {}
+        for survey, name in self.teacher_surveys.items():
+            pk = self.cleaned_data.get(name, "-1")
+            if pk != "-1":
+                self.survey2teacher[survey] = Teacher.objects.get(pk=self.cleaned_data[name])
 
         return self.cleaned_data
+
+    def save_surveys(self):
+        used = set()
+        for entry in StudentSurveyEntry.objects.filter(student=self.instance):
+            if entry.survey in self.survey2student:
+                entry.choice = self.survey2student[entry.survey]
+                entry.save()
+            else:
+                entry.delete()
+            used.add(entry.survey)
+        for survey, choice in self.survey2student.items():
+            if not survey in used:
+                entry = StudentSurveyEntry()
+                entry.survey = survey
+                entry.student = self.instance
+                entry.choice = choice
+                entry.save()
+        
+        used = set()
+        for entry in TeacherSurveyEntry.objects.filter(student=self.instance):
+            if entry.survey in self.survey2teacher:
+                entry.choice = self.survey2teacher[entry.survey]
+                entry.save()
+            else:
+                entry.delete()
+            used.add(entry.survey)
+        for survey, choice in self.survey2teacher.items():
+            if not survey in used:
+                entry = TeacherSurveyEntry()
+                entry.survey = survey
+                entry.student = self.instance
+                entry.choice = choice
+                entry.save()
 
 @login_required
 def edit(request):
@@ -71,6 +127,7 @@ def edit(request):
     if request.method == "POST":
         if form.is_valid():
             form.save()
+            form.save_surveys()
         else:
             messages.error(request, "Konnte Daten nicht speichern!")
     context["form"] = form
